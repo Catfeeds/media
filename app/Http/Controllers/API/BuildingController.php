b0e6f023d713b386a2bffae21ad8cff549673941
jacklin <361525737@qq.com>
2018-04-16 20:54:39 +0800

    完成楼盘楼栋的数据

--- app/Http/Controllers/API/BuildingController.php
+++ app/Http/Controllers/API/BuildingController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Requests\API\BuildingRequest;
+use App\Models\Area;
 use App\Models\Building;
+use App\Models\City;
+use App\Models\Street;
 use App\Repositories\BuildingRepository;
 
 class BuildingController extends APIBaseController
@@ -35,8 +38,54 @@ class BuildingController extends APIBaseController
     }
 
 
+    /**
+     * 说明：所有楼盘下拉数据
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @author jacklin
+     */
     public function buildingSelect()
     {
-        
+        $cities = City::all();
+        $city_box = array();
+        foreach ($cities as $index => $city) {
+            // 循环城市 将区域的
+            $areas = Area::where('city_id', $city->id)->get();
+            $area_box = array();
+            foreach ($areas as $area) {
+                $streets = Street::where('area_id', $area->id)->get();
+                $street_box = array();
+                foreach ($streets as $street) {
+                    $buildings = Building::where('street_id', $street->id)->get();
+                    $building_box = array();
+                    foreach ($buildings as $building) {
+                        $item = array(
+                            'value' => $building->id,
+                            'label' => $building->name,
+                        );
+                        $building_box[] = $item;
+                    }
+                    $item = array(
+                        'value' => $street->id,
+                        'label' => $street->name,
+                        'children' => $building_box
+                    );
+                    $street_box[] = $item;
+                }
+                $item = array(
+                    'value' => $area->id,
+                    'label' => $area->name,
+                    'children' => $street_box
+                );
+                $area_box[] = $item; // 城市下的区
+            }
+            $city_item = array(
+                'value' => $city->name,
+                'label' => $city->name,
+                'children' => $area_box
+            );
+            $city_box[] = $city_item; // 所有城市
+        }
+        return $this->sendResponse($city_box, '获取成功');
     }
 }
