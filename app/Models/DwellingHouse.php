20afc2cc313cb9712d1b06e996c44e5afa76e152
luozhen <15902773450@163.com>
2018-04-16 10:14:24 +0800

    住宅房源

---
+++ app/Models/DwellingHouse.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class DwellingHouse extends BaseModel
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,42 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::group(['domain' => 'admin.agency.com', 'namespace' => 'API'], function () {
+
+    /*
+    |--------------------------------------------------------------------------
+    | 登录后的操作
+    |--------------------------------------------------------------------------
+    */
+//    Route::group(['middleware' => ''], function () {
+
+        Route::get('/test', function () {
+            return '123';
+        });
+
+
+        Route::resource('dwelling_houses', 'DwellingHousesController');
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | 租户
+        |--------------------------------------------------------------------------
+        */
+        Route::resource('user', 'UserController');
+        // 租户 我的设置
+        Route::get('/userSetting', 'UserController@userSetting');
+        // 修改用户姓名
+        Route::get('/updateRealName', 'UserController@updateRealName');
+        // 修改用户图像
+        Route::post('/updateUserHeaderPic', 'UserController@updateUserHeaderPic');
+
+//    });
 });
+
+
