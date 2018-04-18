132d14abdbf55d7ebe49b1a8ab000152c200667d
jacklin <361525737@qq.com>
2018-04-18 09:10:30 +0800

    添加客户和关联表

--- database/migrations/2018_04_13_101604_create_customs_table.php
+++ database/migrations/2018_04_13_101604_create_customs_table.php
@@ -15,6 +15,20 @@ class CreateCustomsTable extends Migration
     {
         Schema::create('customs', function (Blueprint $table) {
             $table->increments('id');
+
+            // 客源状态
+            // 客户等级
+            // 客户来源
+            // 公司盘
+            // 客户姓名
+            // 客户电话
+            // 联系人
+            // 承受价格 n 元 到 n元
+            // 付款房还是
+            // 付佣
+            // 租房类型
+
+            // 客户需求
             $table->timestamps();
             $table->softDeletes();
         });
