5423ee8ec88543af87d8cb353ba35fc868260395
luozhen <15902773450@163.com>
2018-04-18 20:37:00 +0800

    跟进添加

--- database/migrations/2018_04_13_101619_create_tracks_table.php
+++ database/migrations/2018_04_13_101619_create_tracks_table.php
@@ -15,6 +15,13 @@ class CreateTracksTable extends Migration
     {
         Schema::create('tracks', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('house_id')->nullable()->comment('房源id');
+            $table->integer('custom_id')->nullable()->comment('房源id');
+
+            $table->string('tracks_mode', 32)->nullable()->comment('跟进方式');
+            $table->string('tracks_mode', 32)->nullable()->comment('跟进方式');
+
+
             $table->timestamps();
             $table->softDeletes();
         });
