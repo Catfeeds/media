cadaf652641b11419105b07b39e7ca90f71c5de3
jacklin <361525737@qq.com>
2018-04-26 14:57:54 +0800

    添加城市的seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionGroupSeeder::class);
         $this->call(PermissionSeeder::class);
         $this->call(RolesSeeder::class);
+        $this->call(CitySeeder::class);
     }
 }
