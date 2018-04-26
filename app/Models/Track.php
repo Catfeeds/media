d7f24d4d49f46e8eac719a13d9d6b939a23ffea7
lkt <lkuntao@163.com>
2018-04-26 12:44:13 +0000

    获取跟进记录表

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -7,5 +7,49 @@ class Track extends BaseModel
 {
     protected $casts = [
         'tracks_time' => 'array',
+
     ];
+
+    protected $appends = ['tracks_mode_label','custom_name'];
+
+    public function custom()
+    {
+        return $this->belongsTo(Custom::class);
+    }
+
+    public function getTracksModeLabelAttribute()
+    {
+        switch ($this->tracks_mode) {
+            case 1:
+                return '实勘';
+            case 2:
+                return '电话';
+            case 3:
+                return '短信';
+            case 4:
+                return '来访';
+            case 5:
+                return '拜访';
+            case 6:
+                return '约看';
+            case 7:
+                return '看房';
+            case 8:
+                return '签约';
+            case 9:
+                return '拿钥匙';
+            case 10:
+                return '续约';
+            case 11:
+                return '重激活';
+             default;
+                break;
+        }
+    }
+
+    public function getCustomNameAttribute()
+    {
+        if (empty($this->custom)) return '';
+        return $this->custom->name;
+    }
 }
