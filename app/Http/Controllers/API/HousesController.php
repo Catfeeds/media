b220e87d93c337f34e4cd40cc871c5e3d3064720
jacklin <361525737@qq.com>
2018-05-07 11:28:58 +0800

    添加扫码页面

--- app/Http/Controllers/API/HousesController.php
+++ app/Http/Controllers/API/HousesController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Requests\API\HousesRequest;
 use App\Services\HousesService;
+use Illuminate\Http\Request;
 
 class HousesController extends APIBaseController
 {
@@ -30,5 +31,20 @@ class HousesController extends APIBaseController
         // 获取查看记录
         $viewRecord = $housesService->getViewRecord($house, $request->per_page);
         return $this->sendResponse( ['owner_info' => $ownerInfo, 'view_record' => $viewRecord],'业主信息,查看记录获取成功');
+    }
+
+    public function houseImgUpdateView(Request $request)
+    {
+        // 判断是否本人操作（header 的 token，和 当前用户的token）
+        // 没有house 返回错误
+        // 失效时间判断 2分钟
+        return view('agency.house.img_house_update');
+    }
+
+    public function houseImgUpdate()
+    {
+        // 判断是否本人操作
+        // 没有house 返回错误
+        // 更新house的图片
     }
 }
