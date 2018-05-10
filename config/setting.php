58cfade9da8e5798d462c20d0c4f838e573aa2a8
jacklin <361525737@qq.com>
2018-05-10 17:38:09 +0800

    修改配置文件

--- config/setting.php
+++ config/setting.php
@@ -11,8 +11,8 @@ return [
     'qiniu_secret_key' => 'Gb2K_HZbepbu-A45y646sP1NNZF3AqzY_w680d5h', // 七牛访问秘钥
 
     // 开发 七牛存储空间
-    'qiniu_bucket' => 'louwang-test',
-    'qiniu_url' => 'http://osibaji20.bkt.clouddn.com/',               // 七牛访问url
+    'qiniu_bucket' => env('QINIU_BUCKET'),     // 七牛访问url
+    'qiniu_url' => env('QINIU_URL'),
 
     /*
     |--------------------------------------------------------------------------
