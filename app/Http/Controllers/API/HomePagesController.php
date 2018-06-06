a8268a4829a7036c9395f0e939bee5e1c9a36f3f
刘坤涛 <lkuntao@163.com>
2018-06-06 11:12:32 +0000

    图表数据

--- app/Http/Controllers/API/HomePagesController.php
+++ app/Http/Controllers/API/HomePagesController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomePagesController extends APIBaseController
 {
     /**
-     * 说明: 后台首页数据
+     * 说明: 后台首页新增数据
      *
      * @param HomePagesService $service
      * @param Request $request
@@ -89,5 +89,59 @@ class HomePagesController extends APIBaseController
     {
         $res = $service->customerStatistic($request->class);
         return $this->sendResponse($res, '客户统计数据获取成功');
+    }
+
+    /**
+     * 说明: 获取房源或客源环比数据
+     *
+     * @param HomePagesService $service
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @author 刘坤涛
+     */
+    public function getRingThanData
+    (
+        HomePagesService $service,
+        Request $request
+    )
+    {
+        $res = $service->getRingThanData($request->model);
+        return $this->sendResponse($res, '环比数据获取成功');
+    }
+
+    /**
+     * 说明: 获取房源、客源图表数据
+     *
+     * @param HomePagesService $service
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @author 刘坤涛\
+     */
+    public function getChartData
+    (
+        HomePagesService $service,
+        Request $request
+    )
+    {
+        $res = $service->getChartData($request->model, $request->time);
+        return $this->sendResponse($res, '图表数据获取成功');
+    }
+
+    /**
+     * 说明: 根据登录人等级查看对应业务员数据
+     *
+     * @param HomePagesService $service
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @author 刘坤涛
+     */
+    public function getSalesmanData
+    (
+        HomePagesService $service,
+        Request $request
+    )
+    {
+        $res = $service->getSalesmanData($request);
+        return $this->sendResponse($res, '获取成功');
     }
 }
