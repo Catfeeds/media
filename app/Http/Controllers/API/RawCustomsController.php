6f70f3a39acf1148984e268a174950bba58b1f4c
刘坤涛 <lkuntao@163.com>
2018-06-15 01:39:30 +0000

    微信

--- app/Http/Controllers/API/RawCustomsController.php
+++ app/Http/Controllers/API/RawCustomsController.php
@@ -30,7 +30,18 @@ class RawCustomsController extends APIBaseController
     )
     {
         $res = $repository->addRawCustom($request, $service);
-
+        $wechat = app('wechat');
+        dd($wechat);
+        $url= 'http://www.baidu.com';
+        $data = array(
+            'first' => array('您好,您有新的客户', '#555555'),
+            'keyword1' => array('贾','#336699') ,
+            'keyword2' => array('110','#ff0000'),
+            'keyword3' => array('写字楼租赁','#888888'),
+            'keyword4' => array(date('Y-m-d H:i:s',time()),'#888888'),
+            'remark'   => array('感谢您的使用','#5599ff')
+        );
+        $wechat->notice->to('oPRyPwyGIy7pf2Ei-xG1lNjHdmo4')->uses('9byKW2NiFk4PoLsdZF4WATd-okTuUIwk0nZK0XeYoHE')->andUrl($url)->data($data)->send();
         return $this->sendResponse($res, '客户录入成功');
     }
 
